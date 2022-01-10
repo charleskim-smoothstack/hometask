61454e62f4e244e17605cfad27e007a56185c821
Charles Kim <chakim013@gmail.com>
2022-01-10 04:31:05 -0500

    Add test for Service for data source server error

--- src/test/java/com/charleskim/hometask/controller/AccountServiceTest.java
+++ src/test/java/com/charleskim/hometask/controller/AccountServiceTest.java
@@ -2,6 +2,7 @@ package com.charleskim.hometask.controller;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -20,7 +21,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootTest
 class AccountServiceTest {
@@ -112,5 +115,21 @@ class AccountServiceTest {
         ValidationResponse expectedValidationResponse = new ValidationResponse();
         expectedValidationResponse.addValidationResult(dataSourceUrl, isValid);
         assertThat(validationResponse, is(expectedValidationResponse));
+    }
+
+    @Test
+    void getAccountValidation_BadSource_ResponseStatusException() {
+        Long accountNumber = 1L;
+        AccountInfo accountInfo = new AccountInfo(accountNumber);
+        RequestEntity<AccountInfo> requestEntity = RequestEntity.post(dataSourceUrl)
+                .contentType(MediaType.APPLICATION_JSON).body(accountInfo);
+        when(restTemplate.exchange(requestEntity, AccountStatus.class))
+                .thenThrow(new RestClientException("Server failed."));
+
+        ValidationRequest validationRequest = new ValidationRequest();
+        validationRequest.setAccountNumber(accountNumber);
+        assertThrows(ResponseStatusException.class, () -> {
+            accountService.getAccountValidation(validationRequest);
+        });
     }
 }
